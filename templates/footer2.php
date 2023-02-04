0e68f81ab7915f8530b0299eeee4c7a9c416d4d7
Marija Rajic <marijarajic64@gmail.com>
2023-02-04 23:58:35 +0100

    Napravljene stranice login i registration

---
+++ templates/footer2.php
@@ -0,0 +1,4 @@
+<footer style="padding-top: 70px">
+    <div class="center black-text text-darken-2"><b>&copy; FILM REVIEW 2023</b></div>
+</footer>
+</body>
--- templates/header.php
+++ templates/header.php
@@ -13,6 +13,8 @@ if (isset($_POST['logout'])) {
     <link rel="icon" href="images/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 
 
